b2b2250e4297045f491066e083ac8e125e3ee3c6
Ryan Wiener <ryanl.wiener@yahoo.com>
2017-07-10 20:23:46 -0700

    Fix landing

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+header("Location: https://roastr2.herokuapp.com/login.html");
+/*
 $conn = mysqli_connect("Enter database info here");
 If ($conn)
 {
@@ -9,4 +11,5 @@ else
 	die("Connection failed: " . mysqli_connect_error());
 }
 mysqli_close($conn);
+ */
 ?>
