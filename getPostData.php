d47857686f355b65d869d010dde0cbe8678e4d23
Ryan Wiener <ryanl.wiener@yahoo.com>
2017-07-17 21:16:46 -0700

    Testing connection to new db

--- getPostData.php
+++ getPostData.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("roastr.cwskii6ncohr.us-west-2.rds.amazonaws.com", "root", "Patrick4", "roastr", "3306");
+$conn = pg_connect("host=ec2-107-21-205-25.compute-1.amazonaws.com port=5432 dbname=d4v9qcehkq46dq user=lbzclfrlzbwlmj password=2b4aa87b7fa7b7b4761c1e57e540836210b309d95b08853e09ce6158ada6bab9");
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
