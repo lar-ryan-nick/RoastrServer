7a13778c345f450ffc84351b87facb0264e66670
Ryan Wiener <ryanl.wiener@yahoo.com>
2017-07-17 21:13:54 -0700

    Testing connection to new db

--- test.php
+++ test.php
@@ -1,6 +1,11 @@
 <?php
-$like = array();
-$like[0] = "hi";
-$like[1] = "pooper";
-echo $like;
+	$conn = pg_connect("host=ec2-107-21-205-25.compute-1.amazonaws.com port=5432 dbname=d4v9qcehkq46dq user=lbzclfrlzbwlmj password=2b4aa87b7fa7b7b4761c1e57e540836210b309d95b08853e09ce6158ada6bab9");
+	if ($conn)
+	{
+		echo "Lesssssss Goooooooooo";
+	}
+	else
+	{
+		echo "Fuck";
+	}
 ?>
