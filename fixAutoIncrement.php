722df92693a46ecc0f847e1c695574ae2694b9a1
Ryan Wiener <ryanl.wiener@yahoo.com>
2017-07-19 09:08:59 -0700

    Fix auto incrment id

---
+++ fixAutoIncrement.php
@@ -0,0 +1,14 @@
+<?php
+	$conn = pg_connect("host=ec2-107-21-205-25.compute-1.amazonaws.com port=5432 dbname=d4v9qcehkq46dq user=lbzclfrlzbwlmj password=2b4aa87b7fa7b7b4761c1e57e540836210b309d95b08853e09ce6158ada6bab9");
+	if ($conn)
+	{
+		echo "Lesssssss Goooooooooo";
+	}
+	else
+	{
+		echo "Fuck";
+	}
+	$sql = "ALTER SEQUENCE comment_id_seq RESTART WITH 444";
+	pg_query($conn, $sql);
+	pg_close($conn);
+?>
